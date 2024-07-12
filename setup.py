6331d741909911df997300b79dca0d16c2735895
Sriman <109895858+Sriman006@users.noreply.github.com>
2024-07-12 16:07:22 +0530

    new

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup (name= "hate_speech_classification",
+       version= "0.0.1",
+       author= "iNeuron",
+       author_email = "shrisriman06@gmail.com",
+       packages=find_packages(),
+       install_requires=[],
+       )
